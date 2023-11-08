6f21d576b9e80a82ddcda3baf6a315181a25cd70
aoiyamada <aoihana1130@gmail.com>
2023-11-08 09:05:16 +0000

    ログイン後ユーザー表示をする段階まで作成

--- vol10-app/app/account/page.tsx
+++ vol10-app/app/account/page.tsx
@@ -1,9 +1,17 @@
-import { UserProfile } from "@clerk/nextjs";
+import { UserProfile, auth, clerkClient } from "@clerk/nextjs";
 
 export default async function Account() {
+    const { userId } = auth();
+    if (!userId) throw new Error("userId is not found");
+
     return (
         <div>
             <UserProfile />
+            {userId && (
+                <div className="my-16 flex flex-col items-center">
+
+                </div>
+            )}
         </div>
     );
 }
--- vol10-app/app/page.tsx
+++ vol10-app/app/page.tsx
@@ -1,8 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import { getUser } from "./_lib/clerk";
+
 export default async function Home() {
+  const user = await getUser();
 
   return (
     <div className="m-4">
-      <p>こんにちは</p>
+      <div>
+        <p className="mt-4 text-2xl font-semibold">
+          こんにちは！{" "}
+          {user ? (
+            <Link
+              href="/account"
+              className="text-orange-400 hover:border-b-2 hover:border-b-orange-400 "
+            >
+              {user?.firstName}
+              {user?.lastName}
+            </Link>
+          ) : (
+            <span className="text-orange-400">ゲスト</span>
+          )}
+          さん
+        </p>
+      </div>
+      {!user && (
+        <div className="my-6">
+          <Link
+            href="/sign-in"
+            className="rounded bg-blue-500 p-3 text-white hover:bg-blue-400"
+          >
+            サインイン
+          </Link>
+        </div>
+      )}
+      {user && user.profileImageUrl && (
+        <Image
+          src={user.profileImageUrl}
+          width={100}
+          height={100}
+          alt="プロフィール画像"
+          className="my-4"
+        />
+      )}
     </div>
   );
 }
