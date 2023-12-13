1dcdceb2fcf39da8126dd1aa37da067c8b64b920
aoiyamada <aoihana1130@gmail.com>
2023-12-13 03:01:26 +0000

    システムからの返信追記

--- vol10-app/app/_lib/action.ts
+++ vol10-app/app/_lib/action.ts
@@ -22,7 +22,15 @@ export const createChatAction = async (
     message: string
 ) => {
     await createChat(roomId, userId, message);
+
+    // 応答メッセージを生成（ここでは簡単な例として"Hello!"という応答を使用）
+    const responseMessage = "Hello!";
+    // システムからの応答としてメッセージを送信
+    // ここではシステムを表すユーザーIDを0とします（実際のアプリではシステムのユーザーIDに適切なものを設定する）
+    await createChat(roomId, 0, responseMessage);
+    // パスの再検証
     revalidatePath(`/room/${roomId}`);
+
 };
 export const createRoomAction = async (name: string, description: string) => {
     const room = await createRooms(name, description);
