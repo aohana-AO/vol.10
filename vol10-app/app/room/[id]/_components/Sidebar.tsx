c3f9fcb374476d859c3d6b0f52b486a41e9e4911
na2kera <nat3mitaro@gmail.com>
2023-12-13 13:41:35 +0000

    サイドバーデザインの変更

--- vol10-app/app/room/[id]/_components/Sidebar.tsx
+++ vol10-app/app/room/[id]/_components/Sidebar.tsx
@@ -26,17 +26,18 @@ export default function Sidebar() {
   return (
     <div className="fixed mt-1 top-100 left-0 h-screen w-48 bg-gray-800 bg-opacity-80 p-5 z-[50]">
       <button className="relative w-6 h-6" onClick={handleClick}>
-        <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-0.5 bg-white"></span>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+        />
+        <span className="material-symbols-outlined text-white">close</span>
       </button>
       <ul>
-        <li className="text-white border border-white rounded-lg p-2 mb-2">
-          Item 1
-        </li>
-        <li className="text-white border border-white rounded-lg p-2 mb-2">
-          Item 2
-        </li>
-        <li className="text-white border border-white rounded-lg p-2 mb-2">
-          Item 3
+        <li className="text-white border border-white rounded-lg p-2 mb-2 mt-3">
+          <details>
+            <summary>選択したLLM名</summary>
+            <p className="text-xs pl-3">選択した性格と説明</p>
+          </details>
         </li>
       </ul>
     </div>
